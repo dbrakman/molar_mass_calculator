afa178a20f9d330d4d137b585d65953df681dd59
David Brakman <david.brakman@gmail.com>
2018-07-15 16:54:28 -0400

    fixed mistakes, typos in test class. Gitignore PyCharm files

--- tests/test_molar_mass.py
+++ tests/test_molar_mass.py
@@ -7,9 +7,9 @@ def percent_error(observed, actual):
 
 class MolarMassTester(unittest.TestCase):
     """
-    Tests for sensibile behavior, using data from https://www.lfd.uci.edu/~gohlke/molmass/
+    Tests for sensible behavior, using data from https://www.lfd.uci.edu/~gohlke/molmass/
     """
-    ERR_THRESHOLD = .001 / 100  # tolerate up to .001 percent error
+    ERR_THRESHOLD = .1 / 100  # tolerate up to .1 percent error
 
     def test_single_caps(self):
         """
@@ -17,23 +17,23 @@ class MolarMassTester(unittest.TestCase):
         H: 1.008
         """
         result_h = mmc.find_molar_mass('H')
-        actual_h = 1.008
-        self.assertTrue(abs(percent_error(result_h, actual_h)) < .00001)
+        actual_h = 1.007940
+        self.assertTrue(abs(percent_error(result_h, actual_h)) < self.ERR_THRESHOLD)
 
     def test_single_mixed_case(self):
         result = mmc.find_molar_mass('He')
-        actual = 4.00
-        self.assertTrue(abs(percent_error(result, actual)) < .00001)
+        actual = 4.002602
+        self.assertTrue(abs(percent_error(result, actual)) < self.ERR_THRESHOLD)
 
     def test_multi_no_parens(self):
         result = mmc.find_molar_mass('C6H12O6')
         actual = 180.1559
-        self.assertTrue(abs(percent_error(result, actual)) < .00001)
+        self.assertTrue(abs(percent_error(result, actual)) < self.ERR_THRESHOLD)
 
     def test_multi_parens(self):
         result = mmc.find_molar_mass('((PC5H10O8)2(C5H5N5)(C5H6N2O3))1000')
         actual = 735443.2
-        self.assertTrue(abs(percent_error(result, actual)) < .00001)
+        self.assertTrue(abs(percent_error(result, actual)) < self.ERR_THRESHOLD)
 
 
 if '__main__' == __name__:
