f06768fd0093f31e104ee2e32b156f20891a3a37
David Brakman <david@Davids-MacBook-Pro.local>
2018-07-15 13:44:48 -0400

    Initial commit: structure of package; test scaffolding

---
+++ README.rst
@@ -0,0 +1,8 @@
+Molar Mass Calculator
+---------------------
+
+A simple molar mass calculator just to flex on Michael Bonifonte.
+
+GUIs are for scrubs.
+#rekt
+
---
+++ tests/test_molar_mass.py
@@ -0,0 +1,18 @@
+import unittest
+
+class MolarMassTester(unittest.TestCase):
+
+    def test_single_caps(self):
+        self.assertTrue(False)
+
+    def test_single_mixed_case(self):
+        self.assertTrue(False)
+
+    def test_multi_no_parens(self):
+        self.assertTrue(False)
+
+    def test_multi_parens(self):
+        self.assertTrue(False)
+
+if '__main__' == __name__:
+    unittest.main()
